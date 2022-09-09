5d93f0cf5877f609f5cee50718fc8796a4560ab2
hung15102001 <82076329+hung15102001@users.noreply.github.com>
2022-09-09 07:58:13 +0700

    a

---
+++ models/vocabulary.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const vocabularySchema = new mongoose.Schema({
+    // Từ
+    words:{
+        required: true,
+        type: String
+    },
+    // Ý nghĩa
+    meaning:{
+        required: true,
+        type: String
+    },
+    // Loại từ
+    wordForm:{
+        required: true,
+        type: String
+    },
+    // Ảnh
+    images:{
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+export default mongoose.model("Vocabulary", vocabularySchema)
