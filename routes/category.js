dcb1613276f3b4dfd817c8a926503be39647afc8
Vu Tien Long <tllong20002@gmail.com>
2022-09-27 11:19:07 +0700

    DTN-137 feat:new QuestionListenWrite,AnswerListenWrite

--- routes/category.js
+++ routes/category.js
@@ -8,7 +8,7 @@ const routeCategory = express.Router();
 
 routeCategory.get('/categories', listCategories);
 routeCategory.get('/categories/:id', detailCategory);
-routeCategory.post('/categories/:userId',requiredSignin , isAuth, isAdmin, addCategory);
+routeCategory.post('/categories/:userId', addCategory);
 routeCategory.put('/categories/:id', editCategory);
 routeCategory.delete('/categories/:id', removeCategory);
 routeCategory.get('/categories/:id', getCategoryById);
