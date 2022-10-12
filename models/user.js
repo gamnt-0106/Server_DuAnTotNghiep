1cdda58ffc105bd35191d4e790fd06bd67635d50
Vu Tien Long <tllong20002@gmail.com>
2022-10-13 00:16:42 +0700

    DTN-196 refactor:edit-api-signin-signup

--- models/user.js
+++ models/user.js
@@ -1,73 +1,72 @@
 import mongoose from "mongoose";
-import {createHmac} from 'crypto'
+import { createHmac } from "crypto";
 
-
-
-const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required: true,
-        trim:true,
-        lowercase: true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-    phone:{
-        type:String,
-        default:""
+    phone: {
+      type: String,
+      default: "",
     },
-    address:{
-        type:String,
-        default: ""
+    address: {
+      type: String,
+      default: "",
     },
-    img:{
-        type:String,
-        default:""
+    img: {
+      type: String,
+      default: "",
     },
-    sex:{
-        type:Number,
-        default:1
+    sex: {
+      type: Number,
+      default: 1,
+    },
+    colorImage: {
+      type: String,
+      default: "",
     },
     role: {
-        type: String,
-        default: "0"
-    }
-},{timestamps: true})
-
+      type: String,
+      default: "0",
+    },
+    idFacebook: {
+      type: String,
+      default: "",
+    },
+    idGoogle: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
 
 userSchema.methods = {
-
-    // check pass in login
-    authenticate(password){
-        try {
-            return this.password == this.encryPassword(password)
-        } catch (error) {
-            console.log(error);
-        }
-    },
-    encryPassword(password){
-        try {
-            return createHmac("sha256","123456").update(password).digest('hex')
-        } catch (error) {
-            console.log(error);
-        }
+  // check pass in login
+  authenticate(password) {
+    try {
+      return this.password == this.encryPassword(password);
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
+  encryPassword(password) {
+    try {
+      return createHmac("sha256", "123456").update(password).digest("hex");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 
-// userSchema.pre("save", function (next) {
-//     try {
-//         this.password = this.encryPassword(this.password);
-//         next();
-//     } catch (error) {
-//         console.log(error);
-//     }
-// })
-
-
-export default mongoose.model('User',userSchema);
+export default mongoose.model("User", userSchema);
