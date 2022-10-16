64cc9a71327fa2b7ef4cf0791a8484f74cf2a90b
0709nvanh <dungtmph12934@fpt.edu.vn>
2022-10-16 21:07:56 +0700

    api join class for user

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,7 @@ routeAuth.post('/signInnWidthFacebook',signInWidthFaceBook);
 routeAuth.post('/signInWidthGoogle',signInWidthGoogle);
 routeAuth.post('/signup',signUp);
 routeAuth.get('/users/:id',userDetail);
+routeAuth.get('/users',getUser);
 routeAuth.put('/users/:id',updateUser);
 routeAuth.delete('/users/:id',deleteUser);
 routeAuth.get('/users/currentUser/current', getCurrentUser);
