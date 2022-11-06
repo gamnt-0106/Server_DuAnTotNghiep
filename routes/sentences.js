bcfd2471fda9d1bb092401e317de84b4ec02e294
vintph16172 <vintph16172@fpt.edu.vn>
2022-11-06 16:40:02 +0700

    DTN-230 feat: add API get Sentences by Day

--- routes/sentences.js
+++ routes/sentences.js
@@ -1,5 +1,5 @@
 import express  from "express";
-import { addSentence, deleteSentence, editSentence, getDetailSentence, getlistSentence, getlistSentenceByIdActivity } from "../controllers/sentencesController";
+import { addSentence, deleteSentence, editSentence, getDetailSentence, getlistSentence, getlistSentenceByIdActivity, getlistSentenceByIdDay } from "../controllers/sentencesController";
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.put("/sentences/:id", editSentence);
 router.delete("/sentences/:id", deleteSentence);
 
 router.get("/sentences/activity/:id", getlistSentenceByIdActivity);
+router.get("/sentences/day/:dayId", getlistSentenceByIdDay);
 
 export default router;
